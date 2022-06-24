03690885da6af4fafedf0dfe031c4e2888a2b232
Jordibz23 <0333191025@unjfsc.edu.pe>
2022-06-24 08:11:33 -0500

    first commit

---
+++ target/generated-sources/annotations/DatosDO/Datos_.java
@@ -0,0 +1,15 @@
+package DatosDO;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-06-24T08:04:37")
+@StaticMetamodel(Datos.class)
+public class Datos_ { 
+
+    public static volatile SingularAttribute<Datos, String> usuario;
+    public static volatile SingularAttribute<Datos, Integer> id;
+    public static volatile SingularAttribute<Datos, String> contraseña;
+
+}
